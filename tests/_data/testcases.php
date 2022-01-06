17ebf84bf2767eb75c0585907501f6a4cafddf54
Máté Cserép <mcserep@inf.elte.hu>
2022-01-06 03:19:00 +0100

    test: updated test case API tests to handle console arguments properly

--- tests/_data/testcases.php
+++ tests/_data/testcases.php
@@ -5,24 +5,28 @@ return [
         'id' => 1,
         'input' => '1',
         'output' => '1',
+        'arguments' => '1',
         'taskID' => 1
     ],
     'testcase2' => [
         'id' => 2,
         'input' => '2',
         'output' => '4',
+        'arguments' => '8',
         'taskID' => 1
     ],
     'testcase3' => [
         'id' => 3,
         'input' => '3',
         'output' => '9',
+        'arguments' => '27',
         'taskID' => 5
     ],
     'testcase4' => [
         'id' => 4,
         'input' => '3',
         'output' => '9',
+        'arguments' => '27',
         'taskID' => 6
     ]
 ];
