3690455bad5a069729ca2b79b2f2e4cb89c27869
Máté Cserép <mcserep@inf.elte.hu>
2024-06-23 19:02:01 +0200

    chore: added missing OpenAPI spec to CreateUpdateCourseResource

--- modules/admin/resources/CreateUpdateCourseResource.php
+++ modules/admin/resources/CreateUpdateCourseResource.php
@@ -2,9 +2,12 @@
 
 namespace app\modules\admin\resources;
 
+use app\components\openapi\generators\OAItems;
+use app\components\openapi\generators\OAProperty;
+use app\components\openapi\IOpenApiFieldTypes;
 use app\models\Model;
 
-class CreateUpdateCourseResource extends Model
+class CreateUpdateCourseResource extends Model implements IOpenApiFieldTypes
 {
     public string $name;
     public array $codes;
@@ -14,6 +17,14 @@ class CreateUpdateCourseResource extends Model
         return [
             [['name', 'codes'], 'required'],
             ['name', 'string'],
+        ];
+    }
+
+    public function fieldTypes(): array
+    {
+        return [
+            'name' => new OAProperty(['type' => 'integer']),
+            'codes' => new OAProperty(['type' => 'array', new OAItems(['type' => 'string'])]),
         ];
     }
 }
